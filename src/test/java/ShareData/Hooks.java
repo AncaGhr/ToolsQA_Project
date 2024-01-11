8226c91895c3cf79d3e14977850f05f418a34abb
AncaGhr <anca.gherghisan@yahoo.com>
2024-01-11 19:04:23 +0200

    Ultima modificare

--- src/test/java/ShareData/Hooks.java
+++ src/test/java/ShareData/Hooks.java
@@ -18,7 +18,7 @@ public class Hooks extends ShareData { // va avea ca rol sa aduca impreuna toate
     @BeforeMethod
 
     public void prepareEnvironment() {
-        setUp();
+
         testName = this.getClass().getSimpleName();
         PropertyUtility propertyUtility = new PropertyUtility(testName);
         testData = propertyUtility.getAllData();
@@ -31,7 +31,8 @@ public class Hooks extends ShareData { // va avea ca rol sa aduca impreuna toate
     public void clearEnvironment(ITestResult result) {
         if (result.getStatus() == ITestResult.FAILURE) {
             LoggerUtility.error(result.getThrowable().getMessage());
-        } else {
+        }
+        else {
             clear();  // tot asa, apare clear fiindca avem extends ShareData
             LoggerUtility.endTestCase(testName); //apelam METODA STATICA pentru log-uri, nu avem nevoie de obiect
 
